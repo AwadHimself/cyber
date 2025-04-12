741c5f055c4a9e1753e1b55ab4940e467ff4d829
mahmoud <awadhimself1@gmail.com>
2025-04-12 21:19:08 +0200

    filter logic in product page

--- js/products.js
+++ js/products.js
@@ -1,5 +1,6 @@
 let nav= document.querySelector(".header .nav");
 let brandFilter = document.querySelector(".brand-results");
+let filterRresults = document.querySelectorAll(".results");
 
 
 function navTogler() {
@@ -11,26 +12,48 @@ function navTogler() {
   .then(mobiles => {
 
     let mobileBrands = new Set();
+    let mobileBatteryCapacity = new Set();
+    let mobileScreenType = new Set();
+    let mobileScreenDiagonal = new Set();
+    let mobileBuiltInMemory = new Set();
     mobiles.forEach(mobile => {
         mobileBrands.add(mobile.Brand); 
+        mobileBatteryCapacity.add(mobile.BatteryCapacity); 
+        mobileScreenType.add(mobile.ScreenType); 
+        mobileScreenDiagonal.add(mobile.ScreenDiagonal); 
+        mobileBuiltInMemory.add(mobile.BuiltInMemory); 
     });
-    mobileBrands.forEach(brand => {
-        let Filter = document.createElement("div");
-        Filter.classList.add("filter", `filter-${brand}` ,"flex", "justify-start", "gap-2", "items-center", "pt-3");
-        let checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-        checkbox.id = brand;
-        checkbox.value = brand;
-        let label = document.createElement("label");
-        label.setAttribute("for", brand);
-        label.innerHTML = brand;
-        let numOfProducts = document.createElement("span")
-        numOfProducts.innerHTML = `(${mobiles.filter(mobile => mobile.Brand === `${brand}`).length})`;
-        Filter.append(checkbox , label , numOfProducts);
-        brandFilter.append(Filter);
-    });
-      
+    let sortedBatteryCapacity = Array.from(mobileBatteryCapacity).sort((a, b)=> {
+        let numA = parseInt(a);
+        let numB = parseInt(b);
+        return numB - numA;
+      });
+    let sortedScreen = Array.from(mobileScreenDiagonal).sort((a, b)=> {
+        let numA = parseFloat(a);
+        let numB = parseFloat(b);
+        return numB - numA;
+      });
 
+
+    let mobileFilters = [mobileBrands, sortedBatteryCapacity, mobileScreenType, sortedScreen, mobileBuiltInMemory];
+    for(i=0 ; i<mobileFilters.length ; i++){
+        mobileFilters[i].forEach(brand => {
+            let Filter = document.createElement("div");
+            Filter.classList.add("filter", `filter-${brand.replaceAll(' ','-')}` ,"flex", "justify-start", "gap-2", "items-center", "pt-3");
+            let checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
+            checkbox.id = brand;
+            checkbox.value = brand;
+            let label = document.createElement("label");
+            label.setAttribute("for", brand);
+            label.innerHTML = brand;
+            let numOfProducts = document.createElement("span")
+            numOfProducts.innerHTML = `(${
+                mobiles.filter(mobile => mobile[Object.keys(mobiles[0])[i+1]] === `${brand}`).length})`;
+              Filter.append(checkbox , label , numOfProducts);
+            filterRresults[i].append(Filter);
+        });    
+    }
 
     mobiles.forEach(mobile => {
           
