08c4d3500483f5788f20f81de088aabb800353fe
mahmoud <awadhimself1@gmail.com>
2025-04-12 19:16:07 +0200

    filter logic in product page

--- js/products.js
+++ js/products.js
@@ -1,4 +1,6 @@
 let nav= document.querySelector(".header .nav");
+let brandFilter = document.querySelector(".brand-results");
+
 
 function navTogler() {
     nav.classList.toggle("extend")
@@ -7,11 +9,49 @@ function navTogler() {
   fetch("js/mobiles_20.json")
   .then(response => response.json())
   .then(mobiles => {
-      const brands  = ["Samsung", "Apple"];
-      const screenTypes = ["Super Retina XDR OLED", "Super AMOLED"];
+
+    let mobileBrands = new Set();
     mobiles.forEach(mobile => {
-        if(brands.includes(mobile.Brand) && parseInt(mobile.BatteryCapacity) > "" && screenTypes.includes(mobile.ScreenType)){
-            console.log(mobile.ModelName)
+        mobileBrands.add(mobile.Brand); 
+    });
+    mobileBrands.forEach(brand => {
+        let Filter = document.createElement("div");
+        Filter.classList.add("filter", `filter-${brand}` ,"flex", "justify-start", "gap-2", "items-center", "pt-3");
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.id = brand;
+        checkbox.value = brand;
+        let label = document.createElement("label");
+        label.setAttribute("for", brand);
+        label.innerHTML = brand;
+        let numOfProducts = document.createElement("span")
+        numOfProducts.innerHTML = `(${mobiles.filter(mobile => mobile.Brand === `${brand}`).length})`;
+        Filter.append(checkbox , label , numOfProducts);
+        brandFilter.append(Filter);
+    });
+      
+
+
+    mobiles.forEach(mobile => {
+          
+      
+        var brands  = [] 
+        if (brands.length === 0){
+            brands = [mobile.Brand]
+          };
+
+          var screenTypes = [];
+        if (screenTypes.length === 0){
+            screenTypes = [mobile.ScreenType]
+          };
+
+
+        if(brands.includes(mobile.Brand) &&
+         parseInt(mobile.BatteryCapacity) > "" 
+         && screenTypes.includes(mobile.ScreenType) && 
+         parseFloat(mobile.ScreenDiagonal) >= "" &&
+         parseInt(mobile.BuiltInMemory) >= ""){
+            
         }
     });
   });
